e8993a456f39e8b5edd4b0f988f28936291cad48
becky <ioao7712@gmail.com>
2021-04-25 14:38:04 +0800

    hw2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+  var arr = []
+  for (var i=0; i<str.length; i++){
+    arr.splice(0,0,str[i])
+  }
+  console.log(arr.join(''))
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,17 @@
 function join(arr, concatStr) {
-  
+  var str = arr[0]
+  for (var i=1; i<arr.length; i++){
+    str += concatStr + arr[i]
+  }
+  return str
 }
 
 function repeat(str, times) {
-  
+  var ans = ''
+  for (var i=0; i<times; i++){
+     ans += str
+  }
+  return ans
 }
 
 console.log(join(['a'], '!'));
